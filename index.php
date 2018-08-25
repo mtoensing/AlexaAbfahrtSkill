6221416255c3d95bd2a00298e779a4b5c767a416
Marc Tönsing <marc@marc.tv>
2018-08-25 23:33:28 +0200

    mock and at least some infos

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
  */
 class DBJourneyXMLParser {
 	const BAHN_ENDPOINT_URL = 'https://reiseauskunft.bahn.de//bin/stboard.exe/dn?rt=1&time=actual&start=yes&boardType=dep&L=vs_java3&input=';
+	const MOCK = true;
 	private $version = '1.0';
 	public $data = '';
 	public $journeys = '';
@@ -22,7 +23,11 @@ class DBJourneyXMLParser {
 		} else {
 			$this->station     = $station;
 			$this->destination = $destination;
-			$url               = DBJourneyXMLParser::BAHN_ENDPOINT_URL . urlencode( $station );
+			//$url               = DBJourneyXMLParser::BAHN_ENDPOINT_URL . urlencode( $station );
+
+			if ( DBJourneyXMLParser::MOCK ) {
+				$url = 'mock.txt';
+			}
 
 			$this->data = file_get_contents( $url );
 
@@ -45,10 +50,10 @@ class DBJourneyXMLParser {
 		$this->journeys = simplexml_load_string( $xml );
 	}
 
-	public function fixProduct($product){
-		$product = trim($product);
-		$product = substr($product, 0, strpos($product, "#"));
-		$clean_product = preg_replace('/\s+/', '', $product);
+	public function fixProduct( $product ) {
+		$product       = trim( $product );
+		$product       = substr( $product, 0, strpos( $product, "#" ) );
+		$clean_product = preg_replace( '/\s+/', '', $product );
 
 		return $clean_product;
 	}
@@ -63,17 +68,58 @@ class DBJourneyXMLParser {
 		print_r( array_unique( $directions ) );
 	}
 
+	public function getFullDate( $arrival_time ) {
 
+		$timestamp_arrival = strtotime( $arrival_time );
+
+		return date( 'l dS \o\f F Y H:i:s', $timestamp_arrival );
+	}
+
+	public function getRelativeTimeInMinutes( $arrival_time ) {
+		$timestamp_arrival = strtotime( $arrival_time );
+		$now               = strtotime( 'now' );
+
+		if ( $timestamp_arrival > $now ) {
+			$arrival_in_minutes = ( $timestamp_arrival - $now  ) / 60;
+
+			return round( $arrival_in_minutes );
+		} else {
+			return false;
+		}
+	}
+
+	public function isNotGone( $arrival_time ) {
+		$timestamp_arrival = strtotime( $arrival_time );
+		$now               = strtotime( 'now' );
+
+		if ( $timestamp_arrival > $now ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	public function getInfo() {
 		foreach ( $this->journeys as $journey ) {
-			if($journey['targetLoc'] == $this->destination){
-				echo $this->fixProduct($journey['prod']) .' - ' .$journey['fpTime'] . ' - ' . $journey['delay'] . '<Br>';
+			if ( $journey['targetLoc'] == $this->destination ) {
+				if ( $this->isNotGone( $journey['fpTime'] ) ) {
+
+					echo  $this->fixProduct( $journey['prod'] ) .
+					     ' RELATIVE: ' .
+					     $this->getRelativeTimeInMinutes( $journey['fpTime'] ) .
+					     ' FULLDATE: ' .
+					     $this->getFullDate( $journey['fpTime'] ) .
+					     ' DISPLAYTIME ' .
+					     ( $journey['fpTime'] ) .
+					     ' DELAY: ' .
+					     $journey['delay'] .
+					     '<br>';
+				}
 			}
 
 		}
 	}
-	
+
 }
 
 $test = new DBJourneyXMLParser( "Hannover, Kafkastrasse", "Wettbergen, Hannover" );
