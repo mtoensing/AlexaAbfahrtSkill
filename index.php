a5a72c4ad0770704412ad4cc5b30fd58a39e62ae
Marc Tönsing <marc@marc.tv>
2018-08-29 20:49:33 +0200

    Added security check

--- index.php
+++ index.php
@@ -76,10 +76,11 @@ class DBJourneyXMLParser {
 	public $origin = '';
 	public $destination = '';
 	public $destination_only;
-	public $echo_request = '';
+	public $EchoReqObj = '';
 	public $display_supported = false;
 	public $remove_from_output = '';
 	public $replace_in_output = '';
+	public $rawJSON;
 
 	/**
 	 * @param bool $display_supported
@@ -112,12 +113,112 @@ class DBJourneyXMLParser {
 	}
 
 	public function getRequest() {
-		$rawJSON            = file_get_contents( 'php://input' );
-		$echo_request       = json_decode( $rawJSON );
-		$this->echo_request = $echo_request;
+		$rawJSON          = file_get_contents( 'php://input' );
+		$this->rawJSON    = $rawJSON;
+		$EchoReqObj       = json_decode( $rawJSON );
+		$this->EchoReqObj = $EchoReqObj;
 
-		if ( isset( $echo_request->context->System->device->supportedInterfaces->Display ) ) {
+		if ( isset( $EchoReqObj->context->System->device->supportedInterfaces->Display ) ) {
 			$this->setDisplaySupported( true );
+		}
+
+		$this->validateRequest();
+	}
+
+	public function ThrowRequestError( $code = 400, $msg = 'Bad Request' ) {
+		GLOBAL $SETUP;
+		$code = 400;
+		http_response_code( $code );
+		echo "Error " . $code . "<br />\n" . $msg;
+		error_log( "alexa/" . $SETUP['SkillName'] . ":\t" . $msg, 0 );
+		exit();
+	}
+
+
+	public function validateRequest() {
+
+		$SETUP = array(
+			'SkillName'           => "kafkastrasse",
+			'SkillVersion'        => '1.0',
+			'ApplicationID'       => 'amzn1.ask.skill.6f5d7f58-b0c7-4ef4-96c8-dd28418c96ba',
+			// From your ALEXA developer console like: 'amzn1.ask.skill.45c11234-123a-1234-ffaa-1234567890a'
+			'CheckSignatureChain' => true,
+			// make sure the request is a true amazonaws api call
+			'ReqValidTime'        => 60,
+			// Time in Seconds a request is valid
+			'AWSaccount'          => '',
+			//If this is != empty the specified session->user->userId is required. This is usefull for account bound private only skills
+			'validIP'             => false,
+			// Limit allowed requests to specified IPv4, set to FALSE to disable the check.
+			'LC_TIME'             => "de_DE"
+			// We use german Echo so we want our date output to be german
+		);
+
+		$EchoReqObj = $this->EchoReqObj;
+		$rawJSON    = $this->rawJSON;
+
+		// Check if Amazon is the Origin
+		if ( is_array( $SETUP['validIP'] ) ) {
+			$isAllowedHost = false;
+			foreach ( $SETUP['validIP'] as $ip ) {
+				if ( stristr( $_SERVER['REMOTE_ADDR'], $ip ) ) {
+					$isAllowedHost = true;
+					break;
+				}
+			}
+			if ( $isAllowedHost == false ) {
+				$this->$this->ThrowRequestError( 403, "Forbidden, your Host is not allowed to make this request!" );
+			}
+			unset( $isAllowedHost );
+		}
+// Check if correct requestId
+		if ( strtolower( $EchoReqObj->session->application->applicationId ) != strtolower( $SETUP['ApplicationID'] ) || empty( $EchoReqObj->session->application->applicationId ) ) {
+			$this->ThrowRequestError( 401, "Forbidden, unkown Application ID!" );
+		}
+// Check SSL Signature Chain
+		if ( $SETUP['CheckSignatureChain'] == true ) {
+			if ( preg_match( "/https:\/\/s3.amazonaws.com(\:443)?\/echo.api\/*/i", $_SERVER['HTTP_SIGNATURECERTCHAINURL'] ) == false ) {
+				$this->ThrowRequestError( 403, "Forbidden, unkown SSL Chain Origin!" );
+			}
+			// PEM Certificate signing Check
+			// First we try to cache the pem file locally
+			$local_pem_hash_file = sys_get_temp_dir() . '/' . hash( "sha256", $_SERVER['HTTP_SIGNATURECERTCHAINURL'] ) . ".pem";
+			if ( ! file_exists( $local_pem_hash_file ) ) {
+				file_put_contents( $local_pem_hash_file, file_get_contents( $_SERVER['HTTP_SIGNATURECERTCHAINURL'] ) );
+			}
+			$local_pem = file_get_contents( $local_pem_hash_file );
+			if ( openssl_verify( $rawJSON, base64_decode( $_SERVER['HTTP_SIGNATURE'] ), $local_pem ) !== 1 ) {
+				$this->ThrowRequestError( 403, "Forbidden, failed to verify SSL Signature!" );
+			}
+			// Parse the Certificate for additional Checks
+			$cert = openssl_x509_parse( $local_pem );
+			if ( empty( $cert ) ) {
+				$this->ThrowRequestError( 424, "Certificate parsing failed!" );
+			}
+			// SANs Check
+			if ( stristr( $cert['extensions']['subjectAltName'], 'echo-api.amazon.com' ) != true ) {
+				$this->ThrowRequestError( 403, "Forbidden! Certificate SANs Check failed!" );
+			}
+			// Check Certificate Valid Time
+			if ( $cert['validTo_time_t'] < time() ) {
+				$this->ThrowRequestError( 403, "Forbidden! Certificate no longer Valid!" );
+				// Deleting locally cached file to fetch a new at next req
+				if ( file_exists( $local_pem_hash_file ) ) {
+					unlink( $local_pem_hash_file );
+				}
+			}
+			// Cleanup
+			unset( $local_pem_hash_file, $cert, $local_pem );
+		}
+// Check Valid Time
+		if ( time() - strtotime( $EchoReqObj->request->timestamp ) > $SETUP['ReqValidTime'] ) {
+			$this->ThrowRequestError( 408, "Request Timeout! Request timestamp is to old." );
+		}
+// Check AWS Account bound, if this is set only a specific aws account can run the skill
+		if ( ! empty( $SETUP['AWSaccount'] ) ) {
+			if ( empty( $EchoReqObj->session->user->userId ) || $EchoReqObj->session->user->userId != $SETUP['AWSaccount'] ) {
+				$this->ThrowRequestError( 403, "Forbidden! Access is limited to one configured AWS Account." );
+			}
 		}
 	}
 
@@ -126,7 +227,7 @@ class DBJourneyXMLParser {
 		$speech = 'Ich habe keine Informationen zu ' . $this->journeys[0]->product;
 		$text1  = '';
 		$text2  = '';
-		$title = $this->origin . ' in Richtung ' . $this->destination;
+		$title  = $this->origin . ' in Richtung ' . $this->destination;
 
 		$journeys_num = count( $this->journeys );
 
