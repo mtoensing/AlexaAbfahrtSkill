de30c2c5b7d808eca794e189524c76fc9271e41f
Marc Tönsing <marc@marc.tv>
2018-09-06 21:15:10 +0200

    Readded ssl certificate check

--- classes/AlexaAbfahrtenSkill.class.php
+++ classes/AlexaAbfahrtenSkill.class.php
@@ -105,7 +105,7 @@ class AlexaAbfahrtenSkill
         if ($this->debug == false) {
 
             $EchoReqObj = $this->EchoReqObj;
-            //$rawJSON    = $this->rawJSON;
+            $rawJSON    = $this->rawJSON;
             $SETUP = $this->setup;
 
             if ($EchoReqObj == '') {
@@ -144,11 +144,11 @@ class AlexaAbfahrtenSkill
                 }
                 $local_pem = file_get_contents($local_pem_hash_file);
 
-                /*
+
                 if ( openssl_verify( $rawJSON, base64_decode( $_SERVER['HTTP_SIGNATURE'] ), $local_pem ) !== 1 ) {
                     $this->ThrowRequestError( 400, "Forbidden, failed to verify SSL Signature!" );
                 }
-                */
+                
                 // Parse the Certificate for additional Checks
                 $cert = openssl_x509_parse($local_pem);
                 if (empty($cert)) {
